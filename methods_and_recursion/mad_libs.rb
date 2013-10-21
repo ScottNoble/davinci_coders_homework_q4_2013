4e3a9dbef609b2503a292d42169dfd50cb672f1d
Scott Noble <Runwolf21@yahoo.com>
2013-10-21 14:18:40 -0600

    Finished the mad libs program
    [#59021734]
    `

---
+++ methods_and_recursion/mad_libs.rb
@@ -0,0 +1,79 @@
+#!/usr/bin/env ruby
+
+
+#O my Luve's like a red, red rose
+#That’s newly sprung in June;
+#O my Luve's like the melodie
+#That’s sweetly play'd in tune.
+
+#As fair art thou, my bonnie lass,
+#So deep in luve am I:
+#And I will luve thee still, my dear,
+#Till a’ the seas gang dry:
+
+#Till a’ the seas gang dry, my dear,
+#And the rocks melt wi’ the sun:
+#I will luve thee still, my dear,
+#While the sands o’ life shall run.
+
+#And fare thee well, my only Luve
+#And fare thee well, a while!
+#And I will come again, my Luve,
+#Tho’ it were ten thousand mile.
+
+# color
+puts "Anyone can write a poem like the great baird Robby Burns\n Let's get started."
+puts "enter a term of endearment"
+term_of_endearment1 = gets
+puts "enter a color"
+color = gets
+puts "enter a flower"
+flower = gets
+puts "enter a month"
+month = gets
+puts "enter a noun"
+noun = gets
+puts "enter an adjective ending in -ly"
+adjective_ending_in_ly = gets
+puts "enter a past-tense verb"
+past_tense_verb = gets
+puts "enter an adjective"
+adjective2 = gets
+puts "enter an adjective"
+adjective3 = gets
+puts "enter a noun"
+noun2 = gets
+puts "enter a body of water"
+body_of_water = gets
+puts "enter a plural noun"
+plural_noun = gets
+puts "enter a plural noun"
+plural_noun2 = gets
+puts "enter a verb"
+verb = gets
+puts "enter a term of endearment"
+term_of_endearment2 = gets
+puts "enter a number"
+number = gets
+puts "enter a plural unit of measurement"
+unit_of_measurement = gets
+
+puts "O my #{term_of_endearment2}'s like a #{color}, #{color} #{flower}
+That’s newly sprung in #{month};
+O my Luve's like the #{noun}
+That’s #{adjective_ending_in_ly} #{past_tense_verb} in tune.
+
+As #{adjective2} art thou, my #{adjective3} lass,
+So #{adjective3} in luve am I:
+And I will luve thee still, my #{noun2},
+Till a’ the #{body_of_water} gang dry:
+
+Till a’ the #{body_of_water} gang dry, my #{term_of_endearment1},
+And the #{plural_noun} melt wi’ the sun:
+I will luve thee still, my #{term_of_endearment1},
+While the #{plural_noun2} o’ life shall #{verb}.
+
+And fare thee well, my only #{term_of_endearment2}
+And fare thee well, a while!
+And I will come again, my #{term_of_endearment2},
+Tho’ it were #{number} #{unit_of_measurement}."
