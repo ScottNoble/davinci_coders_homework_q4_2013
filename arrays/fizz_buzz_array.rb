ce2d5aff60e59220cd18316be131d1e2fdff1ad9
Scott Noble <Runwolf21@yahoo.com>
2013-10-21 13:12:44 -0600

    Added finished fizz_buzz_array.rb to array directory in my repository
    [#59020858]

--- arrays/fizz_buzz_array.rb
+++ arrays/fizz_buzz_array.rb
@@ -8,6 +8,7 @@
 # but should make a method on class array, this could also be done recursively or done in a loop.
 
 # why do I get an undefined error unless I define the newarray before the each method?
+
 newarray = []
 array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
          20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 
