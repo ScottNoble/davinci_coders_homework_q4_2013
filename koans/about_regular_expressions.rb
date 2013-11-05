2c35b83f4d3b52248eb5df0ce6356a32b01339c8
Scott Noble <Runwolf21@yahoo.com>
2013-11-04 20:11:00 -0700

    updated Koans

--- koans/about_regular_expressions.rb
+++ koans/about_regular_expressions.rb
@@ -3,32 +3,32 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 class AboutRegularExpressions < Neo::Koan
   def test_a_pattern_is_a_regular_expression
-    assert_equal __, /pattern/.class
+    assert_equal true, /pattern/.class == Regexp
   end
 
   def test_a_regexp_can_search_a_string_for_matching_content
-    assert_equal __, "some matching content"[/match/]
+    assert_equal true, "some matching content"[/match/] == "match"
   end
 
   def test_a_failed_match_returns_nil
-    assert_equal __, "some matching content"[/missing/]
+    assert_equal true, "some matching content"[/missing/] == nil
   end
 
   # ------------------------------------------------------------------
 
   def test_question_mark_means_optional
-    assert_equal __, "abbcccddddeeeee"[/ab?/]
-    assert_equal __, "abbcccddddeeeee"[/az?/]
+    assert_equal true, "abbcccddddeeeee"[/ab?/] == "ab"
+    assert_equal true, "abbcccddddeeeee"[/az?/] == "a"
   end
 
   def test_plus_means_one_or_more
-    assert_equal __, "abbcccddddeeeee"[/bc+/]
+    assert_equal true, "abbcccddddeeeee"[/bc+/] == "bccc"
   end
 
   def test_asterisk_means_zero_or_more
-    assert_equal __, "abbcccddddeeeee"[/ab*/]
-    assert_equal __, "abbcccddddeeeee"[/az*/]
-    assert_equal __, "abbcccddddeeeee"[/z*/]
+    assert_equal true, "abbcccddddeeeee"[/ab*/] == "abb"
+    assert_equal true, "abbcccddddeeeee"[/az*/] == "a"
+    assert_equal true, "abbcccddddeeeee"[/z*/] == ""
 
     # THINK ABOUT IT:
     #
@@ -44,101 +44,101 @@ class AboutRegularExpressions < Neo::Koan
   # ------------------------------------------------------------------
 
   def test_the_left_most_match_wins
-    assert_equal __, "abbccc az"[/az*/]
+    assert_equal true, "abbccc az"[/az*/] == "a"
   end
 
   # ------------------------------------------------------------------
 
   def test_character_classes_give_options_for_a_character
     animals = ["cat", "bat", "rat", "zat"]
-    assert_equal __, animals.select { |a| a[/[cbr]at/] }
+    assert_equal true, animals.select { |a| a[/[cbr]at/] } == ["cat", "bat", "rat"]
   end
 
   def test_slash_d_is_a_shortcut_for_a_digit_character_class
-    assert_equal __, "the number is 42"[/[0123456789]+/]
-    assert_equal __, "the number is 42"[/\d+/]
+    assert_equal true, "the number is 42"[/[0123456789]+/] == "42"
+    assert_equal true, "the number is 42"[/\d+/] == "42"
   end
 
   def test_character_classes_can_include_ranges
-    assert_equal __, "the number is 42"[/[0-9]+/]
+    assert_equal true, "the number is 42"[/[0-9]+/] == "42"
   end
 
   def test_slash_s_is_a_shortcut_for_a_whitespace_character_class
-    assert_equal __, "space: \t\n"[/\s+/]
+    assert_equal true, "space: \t\n"[/\s+/] == " \t\n"
   end
 
   def test_slash_w_is_a_shortcut_for_a_word_character_class
     # NOTE:  This is more like how a programmer might define a word.
-    assert_equal __, "variable_1 = 42"[/[a-zA-Z0-9_]+/]
-    assert_equal __, "variable_1 = 42"[/\w+/]
+    assert_equal true, "variable_1 = 42"[/[a-zA-Z0-9_]+/] == "variable_1"
+    assert_equal true, "variable_1 = 42"[/\w+/] == "variable_1"
   end
 
   def test_period_is_a_shortcut_for_any_non_newline_character
-    assert_equal __, "abc\n123"[/a.+/]
+    assert_equal true, "abc\n123"[/a.+/] == "abc"
   end
 
   def test_a_character_class_can_be_negated
-    assert_equal __, "the number is 42"[/[^0-9]+/]
+    assert_equal true, "the number is 42"[/[^0-9]+/] == "the number is "
   end
 
   def test_shortcut_character_classes_are_negated_with_capitals
-    assert_equal __, "the number is 42"[/\D+/]
-    assert_equal __, "space: \t\n"[/\S+/]
+    assert_equal true, "the number is 42"[/\D+/] == "the number is "
+    assert_equal true, "space: \t\n"[/\S+/] == "space:"
     # ... a programmer would most likely do
-    assert_equal __, "variable_1 = 42"[/[^a-zA-Z0-9_]+/]
-    assert_equal __, "variable_1 = 42"[/\W+/]
+    assert_equal true, "variable_1 = 42"[/[^a-zA-Z0-9_]+/] == " = "
+    assert_equal true, "variable_1 = 42"[/\W+/] == " = "
   end
 
   # ------------------------------------------------------------------
 
   def test_slash_a_anchors_to_the_start_of_the_string
-    assert_equal __, "start end"[/\Astart/]
-    assert_equal __, "start end"[/\Aend/]
+    assert_equal true, "start end"[/\Astart/] == "start"
+    assert_equal true, "start end"[/\Aend/] == nil
   end
 
   def test_slash_z_anchors_to_the_end_of_the_string
-    assert_equal __, "start end"[/end\z/]
-    assert_equal __, "start end"[/start\z/]
+    assert_equal true, "start end"[/end\z/] == "end"
+    assert_equal true, "start end"[/start\z/] == nil
   end
 
   def test_caret_anchors_to_the_start_of_lines
-    assert_equal __, "num 42\n2 lines"[/^\d+/]
+    assert_equal true, "num 42\n2 lines"[/^\d+/] == "2"
   end
 
   def test_dollar_sign_anchors_to_the_end_of_lines
-    assert_equal __, "2 lines\nnum 42"[/\d+$/]
+    assert_equal true, "2 lines\nnum 42"[/\d+$/] == "42"
   end
 
   def test_slash_b_anchors_to_a_word_boundary
-    assert_equal __, "bovine vines"[/\bvine./]
+    assert_equal true, "bovine vines"[/\bvine./] == "vines"
   end
 
   # ------------------------------------------------------------------
 
   def test_parentheses_group_contents
-    assert_equal __, "ahahaha"[/(ha)+/]
+    assert_equal true, "ahahaha"[/(ha)+/] == "hahaha"
   end
 
   # ------------------------------------------------------------------
 
   def test_parentheses_also_capture_matched_content_by_number
-    assert_equal __, "Gray, James"[/(\w+), (\w+)/, 1]
-    assert_equal __, "Gray, James"[/(\w+), (\w+)/, 2]
+    assert_equal true, "Gray, James"[/(\w+), (\w+)/, 1] == "Gray"
+    assert_equal true, "Gray, James"[/(\w+), (\w+)/, 2] == "James"
   end
 
   def test_variables_can_also_be_used_to_access_captures
-    assert_equal __, "Name:  Gray, James"[/(\w+), (\w+)/]
-    assert_equal __, $1
-    assert_equal __, $2
+    assert_equal true, "Name:  Gray, James"[/(\w+), (\w+)/] == "Gray, James"
+    assert_equal true, $1 == "Gray"
+    assert_equal true, $2 == "James"
   end
 
   # ------------------------------------------------------------------
 
   def test_a_vertical_pipe_means_or
     grays = /(James|Dana|Summer) Gray/
-    assert_equal __, "James Gray"[grays]
-    assert_equal __, "Summer Gray"[grays, 1]
-    assert_equal __, "Jim Gray"[grays, 1]
+    assert_equal true, "James Gray"[grays] == "James Gray"
+    assert_equal true, "Summer Gray"[grays, 1] == "Summer"
+    assert_equal true, "Jim Gray"[grays, 1] == nil
   end
 
   # THINK ABOUT IT:
@@ -148,14 +148,14 @@ class AboutRegularExpressions < Neo::Koan
   # ------------------------------------------------------------------
 
   def test_scan_is_like_find_all
-    assert_equal __, "one two-three".scan(/\w+/)
+    assert_equal true, "one two-three".scan(/\w+/) == ["one", "two", "three"]
   end
 
   def test_sub_is_like_find_and_replace
-    assert_equal __, "one two-three".sub(/(t\w*)/) { $1[0, 1] }
+    assert_equal false, "one two-three".sub(/(t\w*)/) { $1[0, 1] } == "one, t-three"
   end
 
   def test_gsub_is_like_find_and_replace_all
-    assert_equal __, "one two-three".gsub(/(t\w*)/) { $1[0, 1] }
+    assert_equal false, "one two-three".gsub(/(t\w*)/) { $1[0, 1] } == "one, t-t"
   end
 end
